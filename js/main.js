53170a269c7090ad78f428950eb83815daf88fed
rotosiri-zu <55967256+rotosiri-zu@users.noreply.github.com>
2019-12-06 16:50:27 +0900

    JavaScriptでパネルを生成しよう

---
+++ js/main.js
@@ -0,0 +1,20 @@
+'use streict'
+
+{
+  class Panel {
+    constructor() {
+      this.el = document.createElement('li');
+      this.el.classList.add('pressed');
+    }
+  }
+  class Board {
+    constructor() {
+      this.panels = [];
+      for (let i = 0; i < 4; i++) {
+        this.panels.push(new Panel());
+      }
+    }
+  }
+
+  const board = new Board();
+}
